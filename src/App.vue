2411d361e001c3202375e4f0347bc2f66589f6ea
Carolina Cordero <ccalvarez@gmail.com>
2018-12-26 19:28:49 -0600

    vuesax list

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template>
   <vs-list>
-    <vs-list-header title="En ejecución"></vs-list-header>
-    <vs-list-item
-      v-for="(task, index) in tareas"
-      :key="index"
-      :title="task.description"
-      :subtitle="task.project.name"
-    ></vs-list-item>
-    <vs-list-header title="Pausadas" color="success"></vs-list-header>
+    <vs-list-header title="Group 1"></vs-list-header>
+    <vs-list-item title="Snickerdoodle" subtitle="An excellent companion"></vs-list-item>
+    <vs-list-item title="Sapporo Haru" subtitle="An excellent polish restaurant, quick delivery and hearty, filling meals"></vs-list-item>
+    <vs-list-header title="Group 2" color="success"></vs-list-header>
     <vs-list-item title="Enid's" subtitle="At night a bar, during the day a delicious brunch spot."></vs-list-item>
     <vs-list-item title="Veronika Ossi" subtitle="Has not watched anything recently"></vs-list-item>
   </vs-list>
@@ -28,17 +24,17 @@ export default {
       tareas: null,
     };
   },
-  mounted() {
-    axios
-      .get(process.env.VUE_APP_APIURL)
-      .then(response => {
-        if (response.status === 200) {
-          this.tareas = response.data;
-        } else if (response.status === 204) {
-        }
-      })
-      .catch(error => {});
-  },
+  // mounted() {
+  //   axios
+  //     .get(process.env.VUE_APP_APIURL + '/users/5c1591a080980742861d7ef6/tasks')
+  //     .then(response => {
+  //       if (response.status === 200) {
+  //         this.tareas = response.data;
+  //       } else if (response.status === 204) {
+  //       }
+  //     })
+  //     .catch(error => {});
+  // },
 };
 </script>
 
